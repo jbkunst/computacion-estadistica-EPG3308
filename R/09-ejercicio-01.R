702c0432c79807188b9d7f91b3fb898ce623b111
Joshua Kunst <jbkunst@gmail.com>
2023-05-17 20:14:44 -0400

    subiendo scropts

--- R/09-ejercicio-01.R
+++ R/09-ejercicio-01.R
@@ -1,3 +1,30 @@
+# ejercicio 1.1 -----------------------------------------------------------
+valor_futuro <- function(captial, interes_anual, meses){
+  interes_mensual <- interes_anual/12
+  captial * (1 + interes_mensual * meses)
+}
+
+# evaluar la función `valor_futuro`
+valor_futuro(100000, 12, 24)
+
+valor_futuro(captial = 100000, interes_anual = 12, meses = 24)
+
+# uno puede cambiar el orden de los argumentos
+# siempre y cuando se nombren
+valor_futuro(interes_anual = 12, meses = 24, captial = 100000)
+
+valor_futuro_im <- function(captial, interes_mensual, meses){
+  captial * (1 + interes_mensual * meses)
+}
+
+valor_futuro_im(100000, 1, 24)
+
+valor_futuro_im <- function(captial, interes_mensual, meses){
+  valorfuturo <- captial * (1 + interes_mensual * meses)
+  valorfuturo
+}
+
+# ejercicio 1.2 -----------------------------------------------------------
 saludo <- function(){
   return("Hola")
 }
@@ -10,9 +37,8 @@ saludo_custom_default <- function(nombre = "Desconocid(a|o|e)"){
   paste("Hola", nombre)
 }
 
-saludo() 
+saludo()
 saludo("Tulio")
-
 
 saludo_custom("Juanin") 
 saludo_custom()
@@ -20,3 +46,7 @@ saludo_custom()
 
 saludo_custom_default("Bodoque")
 saludo_custom_default()
+
+
+
+
--- R/09-ejercicio-02.R
+++ R/09-ejercicio-02.R
@@ -2,8 +2,8 @@
 library(tidyverse)
 library(datos)
 
-fs::dir_create("data/datos_credito_v1")
-fs::dir_create("data/datos_credito_v2")
+fs::dir_create("data/datos_credito")
+# fs::dir_create("data/datos_credito_v2")
 
 datos_credito <- as_tibble(datos_credito)
 
@@ -12,4 +12,94 @@ datos_credito <- datos_credito |>
   sample_frac() |> 
   rename_all(str_to_lower)
 
-datos_credito
+
+# v1 separar los datos y guardarlos en archivos independientes ------------
+# (omitamos NA por simplicidad)
+trabajos <- datos_credito |> 
+  count(trabajo) |> 
+  pull(trabajo) |> 
+  na.omit() |> 
+  as.character()
+
+# walk(trabajos, genere el data set y lo guarde)
+
+map(trabajos, function(t = "freelance"){
+  
+  message(t)
+  
+  daux <- datos_credito |> 
+    filter(trabajo == t)
+  
+  fn <- str_glue("data/datos_credito/dc_v1_{ t }.csv")
+  
+  write_csv(daux, fn)
+ 
+})
+
+# otra opcion de v1
+walk(trabajos, function(t = "freelance"){
+  
+  cli::cli_progress_step("Trabajo: { t }")
+  
+  daux <- datos_credito |> 
+    filter(trabajo == t)
+  
+  fn <- str_glue("data/datos_credito/dc_v1_{ t }.csv")
+  
+  write_csv(daux, fn)
+  
+})
+
+
+# v2 para serparar archivos por columnas ----------------------------------
+nombres <- names(datos_credito)
+nombres
+
+grupos <- split(nombres, ceiling(seq_along(nombres)/4))
+grupos
+
+walk(grupos, function(g = grupos[[3]]){
+  
+  cli::cli_progress_step("Grupo: { g }")
+  
+  variables <- unique(c("id", g))
+  
+  daux <- datos_credito |> 
+    select(all_of(variables))
+  
+  g2 <- str_c(g, collapse = "_")
+  
+  fn <- str_glue("data/datos_credito/dc_v2_{ g2 }.csv")
+  fn 
+  
+  write_csv(daux, fn)
+  
+})
+
+
+# leer cada version de archivo --------------------------------------------
+# v1
+dc1 <- fs::dir_ls("data/datos_credito/") |> 
+  str_subset("_v1_") |> 
+  map(read_csv) |> 
+  reduce(bind_rows)
+
+# v2
+dc2 <- fs::dir_ls("data/datos_credito/") |> 
+  str_subset("_v2_") |> 
+  map(read_csv) |> 
+  reduce(full_join)
+
+# idea markdown -----------------------------------------------------------
+walk(trabajos, function(t = "freelance"){
+  
+  cli::cli_progress_step("Trabajo: { t }")
+  
+  daux <- datos_credito |> 
+    filter(trabajo == t)
+  
+  fn <- str_glue("rmarkdown/informe_{ t }.html")
+  
+  rmarkdown::render("informe.Rmd", fn, params = list(tr = t))
+  
+})
