2841729eac4dcea9767d1bc38bb749bb82391ba8
Joshua Kunst <jbkunst@gmail.com>
2023-06-08 01:07:51 -0400

    ayayai

--- R/12-ejercicio-01-02.R
+++ R/12-ejercicio-01-02.R
@@ -3,64 +3,50 @@ library(tidyverse)
 library(datos)
 library(skimr)
 library(rsample)
-library(partykit)
-library(rpart)
 library(ranger)
+library(rpart)
+library(partykit)
+library(celavi)
 
+# datos --------------------------------------------------------------------
+datos <- datos_credito
 
-# install.packages()
-# pacma
-
-# data --------------------------------------------------------------------
-glimpse(datos_credito)
-
-datos_credito <- filter(datos_credito, complete.cases(datos_credito))
-datos_credito <- janitor::clean_names(datos_credito)
-datos_credito <- datos_credito |> 
+datos <- filter(datos, complete.cases(datos))
+datos <- janitor::clean_names(datos)
+datos <- datos |> 
   mutate(estado = as.numeric(estado == "bueno")) 
 
+glimpse(datos)
 
-glimpse(datos_credito)
+skimr::skim(datos)
 
-class(datos_credito)
+datos <- as_tibble(datos)
 
-skimr::skim(datos_credito)
+datos
 
-# split 
-class(datos_credito)
-
-datos_credito <- as_tibble(datos_credito)
-
-class(datos_credito)
-
-datos_credito
-
-datos_credito <- datos_credito |> 
+set.seed(123)
+datos <- datos |> 
   mutate(muestra = if_else(runif(n()) < .2, "tst", "trn"))
 
-datos_credito |> count(muestra)
+datos |>
+  count(muestra) |> 
+  mutate(p = n/sum(n))
 
-# datasplit <- initial_split(datos_credito)
-# training(datasplit)
-# testing(datasplit)
-
-datos_credito_trn <- datos_credito |> 
+datos_trn <- datos |> 
   filter(muestra == "trn") |> 
   select(-muestra)
 
 
 # tri ---------------------------------------------------------------------
-# m1 <- rpart(estado~ ., data = datos_credito_trn, control = rpart.control(maxdepth = 4))
+# m1 <- rpart(estado~ ., data = datos_trn, control = rpart.control(maxdepth = 4))
 # plot(m1)
 # text(m1)
 
-m2 <- ctree(estado ~ .,  
-            data = datos_credito_trn, control = ctree_control(maxdepth = 4))
-# plot(m2)
-
+m2 <- ctree(estado ~ ., data = datos_trn, control = ctree_control(maxdepth = 4))
+plot(m2)
 
 # rl ----------------------------------------------------------------------
-m3 <- glm(estado ~ ., data = datos_credito_trn, family = binomial())
+m3 <- glm(estado ~ ., data = datos_trn, family = binomial())
 m3 <- step(m3)
 m3
 
@@ -70,32 +56,57 @@ broom::tidy(m3)
 
 
 # rf ----------------------------------------------------------------------
-m4 <- ranger(estado ~ ., data = datos_credito_trn, probability = TRUE)
+m4 <- ranger(estado ~ ., data = datos_trn, probability = TRUE, max.depth = 10)
 
 # evaluar modelos ---------------------------------------------------------
-datos_credito <- datos_credito |> 
+datos <- datos |> 
   mutate(
-    pred_arb = predict(m2, newdata = datos_credito, type = "prob")[, 2],
-    pred_rl  = predict(m3, newdata = datos_credito, type = "response"),
-    pred_rf  = predict(m4, data = datos_credito, type = "response")$predictions[,2]
+    # como se ejecuta regresion no da 2 columnas
+    pred_arb = partykit:::predict.party(m2, newdata = datos),
+    pred_rl  = predict(m3, newdata = datos, type = "response"),
+    pred_rf  = predict(m4, data = datos, type = "response")$predictions[,1]
   )
 
-datos_credito |> 
-  select(muestra, estado, starts_with("pred_")) |> 
-  # mutate(estado = as.numeric(estado == "bueno")) |> 
-  pivot_longer(cols = starts_with("pred"), names_to = "modelo", values_to = "pred") |> 
-  group_by(muestra, modelo) |> 
+datos |>
+  select(muestra, estado, starts_with("pred_")) |>
+  # mutate(estado = as.numeric(estado == "bueno")) |>
+  pivot_longer(cols = starts_with("pred"), names_to = "modelo", values_to = "pred") |>
+  group_by(muestra, modelo) |>
   summarise(
     auc = Metrics::auc(estado, pred)
-  ) |> 
-  ungroup() |> 
-  pivot_wider(names_from = muestra, values_from = auc)
+  ) |>
+  ungroup() |>
+  pivot_wider(names_from = muestra, values_from = auc) |> 
+  arrange(desc(tst))
 
 
 # vi ----------------------------------------------------------------------
-# datos_credito_tst <- datos_credito |> 
-#   filter(muestra == "tst")
-# 
-# datos_credito_tst
-# 
-# celavi::variable_importance(m4, data = datos_credito_tst)
+datos_tst <- datos |>
+  filter(muestra == "tst") |> 
+  select(-starts_with("pred"), -muestra)
+
+datos_tst
+
+vi_2 <- variable_importance(m2, data = datos_tst, iterations = 100)
+plot(vi_2)
+
+vi_3 <- variable_importance(
+  m3, 
+  data = datos_tst, 
+  iterations = 100,
+  predict_function = function(m, d){  predict(m, newdata = d, type = "response") }
+  )
+plot(vi_3)
+
+vi_4 <- variable_importance(
+  m4, 
+  response = "estado",
+  data = datos_tst, 
+  iterations = 10,
+  predict_function = function(m, d){  predict(m, data = d, type = "response")$predictions[,1] }
+)
+
+plot(vi_4)
+
+plot(vi_2, vi_3, vi_4)
+
