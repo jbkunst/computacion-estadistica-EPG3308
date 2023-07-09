4ed05d26cc1a646f41523ce94e4377d0d53f7355
Joshua Kunst <jbkunst@gmail.com>
2023-07-09 17:05:14 -0400

    examples

---
+++ apps/app.R/app.R
@@ -0,0 +1,20 @@
+library(shiny)
+
+ui <- fluidPage(
+    titlePanel("Old Faithful Geyser Data66"),
+    sliderInput("bins", "Number of bins:", min = 1, max = 50, value = 30, width = "100%"),
+    plotOutput("distPlot")
+)
+
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+        hist(x, breaks = bins, col = 'darkgray', border = 'white',
+             xlab = 'Waiting time to next eruption (in mins)',
+             main = 'Histogram of waiting times')
+    })
+}
+
+shinyApp(ui = ui, server = server, options = list(host = "192.168.237.215"))
